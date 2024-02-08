6fa07e0c08e8865640e84b4ad20db8e9c7ca9cb8
Ken <godsdy0410@naver.com>
2024-02-09 00:13:57 +0900

    interactor

---
+++ Assets/Scripts/interactor.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactor : MonoBehaviour
+{
+    Camera cam;
+
+    private RaycastHit hit;
+    public float layDistance = 4f;
+
+    public void checkInteracting()
+    {
+
+    }
+}
